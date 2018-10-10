83631fbb7453308622e48ae97836bbdde9c20cd0
Gytis Trikleris <gytis@redhat.com>
2018-10-10 13:59:02 +0200

    [SB-938] Add retry support to consumer endpoint (#10)

---
+++ spring-cloud-starter-stream-artemis-it/spring-cloud-starter-stream-artemis-it-e2e/src/test/java/me/snowdrop/stream/binder/artemis/RetryIT.java
@@ -0,0 +1,49 @@
+package me.snowdrop.stream.binder.artemis;
+
+import me.snowdrop.stream.binder.artemis.application.StreamApplication;
+import me.snowdrop.stream.binder.artemis.listeners.FailingStreamListener;
+import me.snowdrop.stream.binder.artemis.sources.StringStreamSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        classes = StreamApplication.class,
+        properties = {
+                // Keep values the same as in FailingStreamListener
+                "spring.cloud.stream.bindings.output.destination=failing-destination",
+                "spring.cloud.stream.bindings.input.destination=failing-destination",
+                "spring.cloud.stream.bindings.input.group=failing-group",
+                "spring.cloud.stream.bindings.input.consumer.back-off-multiplier=1"
+        }
+)
+@Import({ StringStreamSource.class, FailingStreamListener.class })
+public class RetryIT {
+
+    @Autowired
+    private StringStreamSource source;
+
+    @Autowired
+    private FailingStreamListener listener;
+
+    @Test
+    public void shouldRetryFailingDeliveries() {
+        source.send("test message");
+
+        await().atMost(5, SECONDS)
+                .until(listener::getErrorsCounter, is(equalTo(1)));
+
+        assertThat(listener.getInvocationsCounter()).isEqualTo(3);
+    }
+
+}
---
+++ spring-cloud-stream-binder-artemis/src/main/java/me/snowdrop/stream/binder/artemis/handlers/RetryableChannelPublishingJmsMessageListener.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2016-2018 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.snowdrop.stream.binder.artemis.handlers;
+
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+import org.springframework.integration.jms.ChannelPublishingJmsMessageListener;
+import org.springframework.retry.RecoveryCallback;
+import org.springframework.retry.support.RetryTemplate;
+
+/**
+ * @author <a href="mailto:gytis@redhat.com">Gytis Trikleris</a>
+ */
+public class RetryableChannelPublishingJmsMessageListener extends ChannelPublishingJmsMessageListener {
+
+    private final RetryTemplate retryTemplate;
+
+    private final RecoveryCallback<?> recoveryCallback;
+
+    public RetryableChannelPublishingJmsMessageListener(RetryTemplate retryTemplate,
+            RecoveryCallback<?> recoveryCallback) {
+        this.retryTemplate = retryTemplate;
+        this.recoveryCallback = recoveryCallback;
+    }
+
+    @Override
+    public void onMessage(javax.jms.Message jmsMessage, Session session) throws JMSException {
+        retryTemplate.execute(c -> {
+            super.onMessage(jmsMessage, session);
+            return null;
+        }, recoveryCallback);
+    }
+
+}
